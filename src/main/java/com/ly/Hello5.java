1460b188ac98f8f4883ae1b3b236c35419273e06
lwmfjc <lwmfjc@gmail.com>
2022-11-07 11:33:06 +0800

    demo

--- src/main/java/com/ly/Hello5.java
+++ src/main/java/com/ly/Hello5.java
@@ -3,16 +3,24 @@ package com.ly;
 import org.openjdk.jol.info.ClassLayout;
 
 import java.util.concurrent.TimeUnit;
+class HelloA{
 
+    synchronized public void say(){
+        System.out.println("synchronized时--start");
+        System.out.println(ClassLayout.parseInstance(this).toPrintable()); //16字节
+        System.out.println("当前线程id(16进制):"+Long.toHexString(Thread.currentThread().getId() ));
+        System.out.println("synchronized时--end");
+    }
+}
 public class Hello5 {
     public static void main(String[] args) throws InterruptedException {
-        Object o = new Object();
         TimeUnit.SECONDS.sleep(5);
-        //System.out.println(Integer.toHexString(o.hashCode()));
-        synchronized (o){
-
-        }
-        System.out.println(ClassLayout.parseInstance(o).toPrintable()); //16字节
+        HelloA helloA = new HelloA();
+        System.out.println("synchronized前");
+        System.out.println(ClassLayout.parseInstance(helloA).toPrintable()); //16字节
+        helloA.say();
+        System.out.println("synchronized后");
+        System.out.println(ClassLayout.parseInstance(helloA).toPrintable()); //16字节
     }
 }
 
