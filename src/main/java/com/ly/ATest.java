088504480c5643dcd1ccb9cb8e31d2ebdcb075cc
lwmfjc <lwmfjc@gmail.com>
2022-12-07 08:48:23 +0800

    测试代码

---
+++ src/main/java/com/ly/ATest.java
@@ -0,0 +1,7 @@
+package com.ly;
+
+public class ATest {
+    public static void main(String[] args) {
+        System.out.println(1 <<2);
+    }
+}
---
+++ src/main/java/com/ly/ThreadLocalDemo.java
@@ -0,0 +1,50 @@
+package com.ly;
+
+import java.lang.reflect.Field;
+
+public class ThreadLocalDemo {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+        Thread t = new Thread(()->test("abc",false));
+        t.start();
+        t.join();
+        System.out.println("--gc后--");
+        Thread t2 = new Thread(() -> test("def", true));
+        t2.start();
+        t2.join();
+    }
+
+    private static void test(String s,boolean isGC)  {
+        try {
+            new ThreadLocal<>().set(s);
+            if (isGC) {
+                System.gc();
+            }
+            Thread t = Thread.currentThread();
+            Class<? extends Thread> clz = t.getClass();
+            Field field = clz.getDeclaredField("threadLocals");
+            field.setAccessible(true);
+            Object ThreadLocalMap = field.get(t);
+            Class<?> tlmClass = ThreadLocalMap.getClass();
+            Field tableField = tlmClass.getDeclaredField("table");
+            tableField.setAccessible(true);
+            //获取ThreadLocalMap的table属性(是一个数组对象)private Entry[] table
+            Object[] arr = (Object[]) tableField.get(ThreadLocalMap);
+            //o为Entry对象
+            for (Object o : arr) {
+                if (o != null) {
+                    Class<?> entryClass = o.getClass();
+                    Field valueField = entryClass.getDeclaredField("value");
+                    //ThreadLocal-->WeakReference-->Reference ,里面有一个属性  private T referent;
+                    //也就是获取Entry对象的referent属性
+                    Field referenceField = entryClass.getSuperclass().getSuperclass().getDeclaredField("referent");
+                    valueField.setAccessible(true);
+                    referenceField.setAccessible(true);
+                    System.out.println(String.format("弱引用key:%s,值:%s", referenceField.get(o), valueField.get(o)));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
