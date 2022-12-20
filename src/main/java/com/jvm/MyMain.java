ef5840bd89247e429d4fda913c6db571765de815
lwmfjc <lwmfjc@gmail.com>
2022-12-20 15:47:28 +0800

    测试代码

--- src/main/java/com/jvm/MyMain.java
+++ src/main/java/com/jvm/MyMain.java
@@ -4,10 +4,15 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class MyMain {
+    private byte[] x = new byte[10 * 1024 * 1024];//10M
+
     public static void main(String[] args) throws InterruptedException {
         System.out.println("开始循环--");
-        while (true){
-            System.out.println("循环中---"+new Date());
+        int i=0;
+        while (++i>0) {
+            String a=new Date().toString();
+            MyMain myMain = new MyMain();
+            System.out.println(i+"循环中---" + new Date());
             TimeUnit.SECONDS.sleep(10);
         }
     }
