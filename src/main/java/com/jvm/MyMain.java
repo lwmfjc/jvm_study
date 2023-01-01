4b5a2a3a1ad8b21dd855a8a0527d3822c8d91d86
lwmfjc <lwmfjc@gmail.com>
2023-01-01 16:38:00 +0800

    类图学习

--- src/main/java/com/jvm/MyMain.java
+++ src/main/java/com/jvm/MyMain.java
@@ -1,12 +1,16 @@
 package com.jvm;
 
+import java.util.AbstractList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MyMain {
     private byte[] x = new byte[10 * 1024 * 1024];//10M
 
     public static void main(String[] args) throws InterruptedException {
+        List list;
+        AbstractList abstractList;
         System.out.println("开始循环--");
         int i=0;
         while (++i>0) {
@@ -15,5 +19,6 @@ public class MyMain {
             System.out.println(i+"循环中---" + new Date());
             TimeUnit.SECONDS.sleep(10);
         }
+
     }
 }
