1ce187a6f60e537a2d8a77ecccf724a346a8be23
lwmfjc <lwmfjc@gmail.com>
2023-01-30 17:30:00 +0800

    测试代码

--- src/main/java/com/test/AtomicTest.java
+++ src/main/java/com/test/AtomicTest.java
@@ -4,7 +4,10 @@ import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class AtomicTest {
     public static void main(String[] args) {
-        AtomicMarkableReference<Object> atomicMarkableReference=new AtomicMarkableReference<>(new Object(),true);
+        //AtomicMarkableReference<Object> atomicMarkableReference=new AtomicMarkableReference<>(new Object(),true);
+        String s2="s1";
+        String s1=new String("s1");
+        System.out.println(s1==s2);
     }
     public class  ABC{
 
