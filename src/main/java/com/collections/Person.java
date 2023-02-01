8b5b5f865894211f85ef38986208a730af678e0d
lwmfjc <lwmfjc@gmail.com>
2023-02-01 16:49:39 +0800

    unsafe类处理

--- src/main/java/com/collections/Person.java
+++ src/main/java/com/collections/Person.java
@@ -3,10 +3,7 @@ package com.collections;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -17,16 +14,9 @@ public class Person {
     // getters and setters
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("a", "b", "c");
-
-
-        List<Person> personList=new ArrayList<>();
-        personList.add(new Person("a1","123"));
-        personList.add(new Person("a2","523"));
-        personList.add(new Person("a3",null));
-        Map<String, String> collect = personList.stream().collect(Collectors.toMap(
-                Person::getName, Person::getPhoneNumber
-        ));
-        System.out.println(collect);
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("abc");
+        System.out.println(arrayList);
+        Map<String,String> map=new HashMap<>();
     }
 }
