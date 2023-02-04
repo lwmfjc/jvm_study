be241902dcd0e9004715a9773e2c3d9fcaaa0ae1
lwmfjc <lwmfjc@gmail.com>
2023-02-04 16:25:22 +0800

    学习

--- src/main/java/com/jvm/MyMain.java
+++ src/main/java/com/jvm/MyMain.java
@@ -1,10 +1,7 @@
 package com.jvm;
 
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class MyMain {
     private byte[] x = new byte[10 * 1024 * 1024];//10M
@@ -16,11 +13,31 @@ public class MyMain {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("abc");
-        },"abcccc");//如果没有"abcccc"，则下面输出null
+            System.out.println("aaa");//输出aaa
+        }, "abcccc");//如果没有"abcccc"，则下面输出null
         FutureTask<Object> futureTask = new FutureTask<>(abc);
-        new Thread(futureTask).start();
+        /*new Thread(futureTask).start();
         Object o = futureTask.get();
-        System.out.println("获取值："+o);
+        System.out.println("获取值："+o); //输出abc
+        */
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<?> future = executorService.submit(futureTask);
+        Future<?> future1 = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "hello";
+            }
+        });
+        /*System.out.println(future.get());//输出null*/
+        System.out.println(future1.get()); //输出hello
+        //System.out.println(futureTask.get());//输出abcccc
+
+        System.out.println("阻塞结束");
+        executorService.shutdown();
     }
 }
