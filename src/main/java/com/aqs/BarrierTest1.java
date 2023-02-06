49bc25811c961fd7c8e6642bbbabb47758895ac8
lwmfjc <lwmfjc@gmail.com>
2023-02-06 13:32:30 +0800

    测试代码

---
+++ src/main/java/com/aqs/BarrierTest1.java
@@ -0,0 +1,68 @@
+package com.aqs;
+
+import java.util.concurrent.*;
+
+public class BarrierTest1 {
+    public static void main(String[] args) throws InterruptedException, TimeoutException, BrokenBarrierException {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                cyclicBarrier.await( );
+                System.out.println("数量11===="+cyclicBarrier.getNumberWaiting());
+                System.out.println("111");
+            } catch (Exception e) {
+                System.out.println("数量异常1111==="+cyclicBarrier.getNumberWaiting());
+                // e.printStackTrace();
+                System.out.println("报错1");
+            }
+
+        });
+        executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                System.out.println("数量2222===="+cyclicBarrier.getNumberWaiting());
+                cyclicBarrier.await(111,TimeUnit.SECONDS);
+                System.out.println("222");
+            } catch (Exception e) {
+                System.out.println("数量异常2222===="+cyclicBarrier.getNumberWaiting());
+                System.out.println("报错2");
+            }
+        });
+        executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                System.out.println("数量33 await前===="+cyclicBarrier.getNumberWaiting());
+                cyclicBarrier.await();
+                System.out.println("数量33 await后===="+cyclicBarrier.getNumberWaiting());
+                System.out.println("333");
+            } catch (Exception e) {
+                System.out.println("数量异常333===="+cyclicBarrier.getNumberWaiting());
+                System.out.println("报错3");
+            }
+        });
+    }
+}
+/*
+ 数量2222====1
+数量33 await前====2
+数量33 await后====0
+333
+数量11====0
+111
+222
+*/
---
+++ src/main/java/com/aqs/SolveABAByAtomicMarkableReference.java
@@ -0,0 +1,38 @@
+package com.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class SolveABAByAtomicMarkableReference {
+
+    private static AtomicMarkableReference atomicMarkableReference = new AtomicMarkableReference(100, false);
+
+    public static void main(String[] args) {
+
+        Thread refT1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            atomicMarkableReference.compareAndSet(100, 101, atomicMarkableReference.isMarked(), !atomicMarkableReference.isMarked());
+            atomicMarkableReference.compareAndSet(101, 100, atomicMarkableReference.isMarked(), !atomicMarkableReference.isMarked());
+        });
+
+        Thread refT2 = new Thread(() -> {
+            //获取原来的marked标记(false)
+            boolean marked = atomicMarkableReference.isMarked();
+            //2s之后进行替换,不应该替换成功
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean c3 = atomicMarkableReference.compareAndSet(100, 101, marked, !marked);
+            System.out.println(c3); // 返回true,实际应该返回false
+        });
+
+        refT1.start();
+        refT2.start();
+    }
+}
