9b89c12e5aca7ca3a5b3d1fe20de6f219fdad2f2
lwmfjc <lwmfjc@gmail.com>
2023-05-30 22:39:02 +0800

    jvm

--- src/main/java/com/Test1.java
+++ src/main/java/com/Test1.java
@@ -1,23 +1,20 @@
 package com;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-class T1{}
-class T2{}
 public class Test1 {
-    public static void main(String[] args) throws SQLException {
-        /*Object t1=new T1();
-        T2 t2=(T2)t1;//java.lang.ClassCastException
-        */
-        /*Thread thread=new Thread(()->{
-            System.out.println("hello");
-        });
-        thread.start();
-        thread.start();*/
-        DriverManager.getDriver("com.MyDriver");
+    public static void main(String[] args)   {
+        //在代码运行之前，常量池已经有"a","2b","c2d" (还有"java")了
+        String  sb1=new StringBuilder("a").append("2b").toString();
+        //常量池中"c2d"字符串的引用是否和sb2是一样的，结果是true;因为
+        //调用sb1.intern()之前，常量池中没有"a2b",所以现在常量池中的a2b其实就是sb1的引用
+        System.out.println(sb1.intern()==sb1);//true
+        String  sb2=new StringBuilder("c2d").toString();
+        //常量池中"c2d"字符串的引用是否和sb2是一样的，结果是false;因为
+        //调用sb2.intern()之前，常量池中就已经有"c2d"
+        System.out.println(sb2.intern()==sb2);//false
+
+        //验证上述猜想
+        String str1 = new StringBuilder("2").append("b").toString();
+        str1.intern();//如果常量池中不存在"2b"，那么就将str1引用保存到常量池;如果存在,则不处理
+        System.out.println("2b".intern()==str1);//false 说明常量池中早就存在"2b"字符串了
     }
 }
