e5b412253a4030421c8b2032ca1cafe84ac0fd89
lwmfjc <lwmfjc@gmail.com>
2023-06-03 00:08:14 +0800

    测试代码

--- src/main/java/com/map/LyMap.java
+++ src/main/java/com/map/LyMap.java
@@ -1,39 +1,22 @@
 package com.map;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
-
-class MyTest{
-    private int age ;
-
-    public MyTest(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MyTest myTest = (MyTest) o;
-        return age == myTest.age;
-    }
-
-    @Override
-    public int hashCode() {
-        return age+10000;
-    }
-}
+import java.util.*;
 public class LyMap {
     public static void main(String[] args) {
-        Map< MyTest,Integer > map= new LinkedHashMap<>();
-        map.put( new MyTest(5),3);
-        map.put(new MyTest(5),5);
-        map.put(new MyTest(5),67);
-        map.put(new MyTest(5),78);
-        map.put(new MyTest(5),22);
-        map.forEach((i,s)->{
-            System.out.println(i);
-        });
+        /*Object[] EMPTY_ELEMENTDATA = {};
+        System.out.println(EMPTY_ELEMENTDATA.length);*/
+
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("a1");
+        arrayList.add("a2");
+        arrayList.add("a3");
+        arrayList.add("a4");
+        arrayList.add("a5");
+        arrayList.add("a6");
+        arrayList.add("a7");
+        arrayList.add("a8");
+        arrayList.add("a9");
+        arrayList.add("a10");
+        arrayList.add("a11");
     }
 }
